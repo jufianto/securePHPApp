27c702d9278de7e87ae53a2b12dbf937534a01ce
jufianto <jufiantohendri@gmail.com>
2017-03-29 18:45:21 +0700

    add secure upload

--- tambah.php
+++ tambah.php
@@ -3,20 +3,55 @@ require_once "config/koneksi.php";
 require_once "safe.php";
 Ceklogin();
 if(isset($_POST['post'])){
-  $gambar = basename($_FILES["gambar"]["name"]);
-  $target_file = "./img/".basename($_FILES["gambar"]["name"]);
+  // $gambar = basename($_FILES["gambar"]["name"]);
+  // $target_file = "./img/".basename($_FILES["gambar"]["name"]);
+  $uploadok = false;
+  $pesan = "";
+//Start
+//Сheck that we have a file
+if((!empty($_FILES["gambar"])) && ($_FILES['gambar']['error'] == 0)) {
+  //Check if the file is JPEG image and it's size is less than 35000Kb
+  $filename = basename($_FILES['gambar']['name']);
+  $ext = substr($filename, strrpos($filename, '.') + 1);
+  if (($ext == "jpg") && ($_FILES["gambar"]["type"] == "image/jpeg") &&
+    ($_FILES["gambar"]["size"] < 35000000)) {
+    //Determine the path to which we want to save this file
+      $newname = dirname(__FILE__).'/img/'.$filename;
+      //Check if the file with the same name is already exists on the server
+      if (!file_exists($newname)) {
+        //Attempt to move the uploaded file to it's new place
+        if ((move_uploaded_file($_FILES['gambar']['tmp_name'],$newname))) {
+          //  echo "It's done! The file has been saved as: ".$newname;
+          $uploadok = true;
+        } else {
+           $pesan =  "Error: A problem occurred during file upload!";
+        }
+      } else {
+         $pesan =  "Error: File ".$_FILES["gambar"]["name"]." already exists";
+      }
+  } else {
+     $pesan =  "Error: Only .jpg images under 35000Kb are accepted for upload";
+  }
+} else {
+ $pesan =  "Error: No file uploaded";
+}
+
+//END
+
+
   $judul = $_POST['judul'];
-  $judulsafe = sqlsafe(Con(),judul);
+  $judulsafe = sqlsafe(Con(),$judul);
   $isi = $_POST['isi'];
   $isisafe = sqlsafe(Con(),$isi);
   $query = "INSERT INTO `post` (`id`,`judul`,`isi`,`gambar`)
-  VALUES (NULL,'$judulsafe','$isisafe','$gambar')";
+  VALUES (NULL,'$judulsafe','$isisafe','$filename')";
     $querya = mysqli_query(Con(),$query);
-    if($querya){
-      move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
+    if($querya && $uploadok){
+      // move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
       header('Location: dashboard.php');
   }else{
-    echo mysqli_error();
+    echo mysqli_error(Con());
+    echo "Pesan = ". $pesan;
   }
 }
 else { ?>
